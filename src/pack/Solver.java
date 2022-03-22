1eea55993a14bace5e6611923eaa4288790fb6b1
jad1223 <100876658+jad1223@users.noreply.github.com>
2022-03-22 09:39:51 -0400

    Create Solver.java

---
+++ src/pack/Solver.java
@@ -0,0 +1,23 @@
+package pack;
+
+public class Solver {
+    public Solver(Board initial){
+
+
+    }
+    public Boolean isSolvable(){
+
+
+    }
+    public int moves(){
+        if(){
+
+
+        }
+        return -1;
+    }
+    public Iterable<Board> solution(){
+
+
+    }
+}
